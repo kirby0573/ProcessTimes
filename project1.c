5d2a0c82c376bc66c54bd709c36d1c19ddf182fd
Peter Nguyen <peter.nguyen0572@gmail.com>
2021-04-04 22:41:40 +0000

    everything but response time

--- project1.c
+++ project1.c
@@ -6,11 +6,16 @@
 typedef struct{
     // <pid> <burst> <priority>	
     int p_info[3];
-    
+    int repeated;
 
 }process;
 
-// don't think I need this method ** 
+float round_float(float f){
+
+    return ((int)(f * 100 + .5) / 100.0);
+}
+
+// method not needed for this proj ** 
 int compare_priority(const void* p1, const void* p2){
     const process *proc1 = p1;
     const process *proc2 = p2;
@@ -28,19 +33,25 @@ float get_throughput(process* p_arr, int size, int p){
     for(int i=0; i < size; i++){
 	total_burst_time += p_arr[i].p_info[1];
     }
-    float rounded = ((int)((p/total_burst_time) * 100 + .5) / 100.0);
+    float rounded = round_float((p/total_burst_time));
     return rounded;
 }
 
 // method to find the number of voluntary switches
 int voluntary_switches(process* p_arr, int size, int p){
-    int switches = 1;
-    for(int i = 0; i < size-1; i++){
-        if(p_arr[i].p_info[0] != p_arr[i+1].p_info[0]){
-            switches++;
+    // want to look for number of unique elements
+    int num_unique = 0;
+    for(int i = 0; i < size; i++){
+	for(int j = i+1; j < size; j++){
+	    if(p_arr[i].p_info[0] == p_arr[j].p_info[0]){
+		p_arr[j].repeated = 1;
+	    }
+	}
+	if(p_arr[i].repeated < 1){
+	    num_unique++;
 	}
     }
-    return switches;
+    return num_unique;
 }
 
 // method to find the number of involuntary switches
@@ -71,7 +82,7 @@ float get_turnaround(process* p_arr, int size, int p, int burst_time){
 
     //turnaround time = total waiting time + total burst time
     total_waiting_time+=burst_time;
-    float rounded = ((int)((total_waiting_time/(p)) * 100 + .5) / 100.0);
+    float rounded = round_float(total_waiting_time/p);
     return rounded;
 }
 
@@ -86,58 +97,64 @@ float get_response_time(process* p_arr, int size, int p){
 }
 
 // method to find the average waiting time
-float get_waiting_time(process* p_arr, int size, int p){
+float get_waiting_time(process* p_arr, int size, int p, int unique){
     int current_time=0;
     float total_waiting_time=0;
-    int idx=1;
     for(int i = 0; i < size-1; i++){
 	if(p_arr[i].p_info[0] != p_arr[i+1].p_info[0]){
 	    current_time += p_arr[i].p_info[1];
-	    idx++;
-	    //printf("switch.. current time = %i\n", current_time);
 	    total_waiting_time+=current_time;
 	}
 	else {
             current_time+=p_arr[i].p_info[1];
         }
     }
-    //printf("%f\n", total_waiting_time);
-    //printf("%d\n",idx);
-    //change time to the num of context switches?
-    float rounded = ((int)((total_waiting_time/idx) * 100 + .5) / 100.0);
+    // 
+    float rounded = round_float((total_waiting_time/unique));
     return rounded;
 }
 
 int main(int argc, char *argv[]){
     
     int P,p,n;
-    scanf("%d",&P);
-    scanf("%d",&p);
-    scanf("%d",&n);
+
+    FILE *input;
+    if(argc > 1){
+	input = fopen(argv[1], "r");
+    } else{
+	input = stdin;
+    }
+    //else{
+    fscanf(input, "%d",&P);
+    fscanf(input, "%d",&p);
+    fscanf(input, "%d",&n);
     int next;
     int idx = 0;
     process* all_processes = calloc(33000, sizeof(process));
 
-    while(scanf("%d",&next) == 1){
+    while(fscanf(input,"%d",&next) == 1){
 	process p;
+	p.repeated = 0;
 	for(int i = 0; i < 3; i++){
 	    p.p_info[i] = next;
 	    if(i < 2){
-	        scanf("%d",&next);
+	        fscanf(input, "%d",&next);
 	    }
 	}
 	all_processes[idx++] = p;
     }
+    //}
     int total_burst = 0;
     for(int i=0; i < n; i++){
         total_burst += all_processes[i].p_info[1];
     }
+    int unique_occurrences = voluntary_switches(all_processes, n, p);
     
-    printf("%i\n", voluntary_switches(all_processes, n, p)-involuntary_switches(all_processes, n, p));
+    printf("%i\n", unique_occurrences);
     printf("%i\n", involuntary_switches(all_processes, n, p));
     printf("100.0\n");
     printf("%.2f\n", get_throughput(all_processes, n, p));
     printf("%.2f\n", get_turnaround(all_processes, n, p, total_burst));
-    printf("%.2f\n", get_waiting_time(all_processes, n, p));
+    printf("%.2f\n", get_waiting_time(all_processes, n, p, unique_occurrences));
     printf("%.2f\n", get_response_time(all_processes, n, p));
 }
